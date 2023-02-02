b336f0c7ac82e9d04e3601d46a3113e7a9d92555
shlomoSandowski <shlomosandowski@gmail.com>
2023-02-02 21:58:37 +0200

    threading

--- backend/formLogic/formStatParser.py
+++ backend/formLogic/formStatParser.py
@@ -1,5 +1,7 @@
+from logging import shutdown
 import formLogic.fieldStatistics as stat
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 def fields_stats(form,fcollection,filled_form):
     field_stats = list()
@@ -9,17 +11,22 @@ def fields_stats(form,fcollection,filled_form):
     affecting_fields =[field["field_name"] for field in form["fields"] if field["field_type"] in ["string_set","int_set","string_mult_set","boolean"]]
     query = {key: filled_form[key] for key in filled_form if key in affecting_fields}
     related_listings = list(fcollection.find(query,{"_id":0}).limit(1000))
+    with ThreadPoolExecutor() as executer:
+        for field in form["fields"]:
+            if field["field_name"] not in filled_form.keys():
+                print(field["field_name"]+" unfilled part")
+                values = [listing.get(field["field_name"]) for listing in related_listings
+                    if listing.get(field["field_name"]) is not None]
+            else:
+                print(field["field_name"]+" filled part")
+                values = [listing.get(field["field_name"]) for listing in unrelated_listings
+                    if listing.get(field["field_name"]) is not None]
 
-    for field in form["fields"]:
-        if field["field_name"] not in filled_form.keys():
-            print(field["field_name"]+" unfilled part")
-            values = [listing.get(field["field_name"]) for listing in related_listings
-                if listing.get(field["field_name"]) is not None]
-        else:
-            print(field["field_name"]+" filled part")
-            values = [listing.get(field["field_name"]) for listing in unrelated_listings
-                if listing.get(field["field_name"]) is not None]
+            field_stats.append({field["field_name"]:executer.submit(stat.get_dist,values,field["field_type"])})
 
-        field_stats.append({field["field_name"]:stat.get_dist(values,field["field_type"])})
+    for field in field_stats:
+        for key in field:
+            field[key] = field[key].result()
+
 
     return field_stats
